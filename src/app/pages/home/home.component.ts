de26557fa493c22f34ff1e9876ce7ab93f737b92
Matheus Kruger <matheus.silva@datapar.com.br>
2021-01-14 16:50:21 -0300

    Adicionando playlists, albuns e musicas salvas do usuário logado na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
+import { User } from './../../models/user';
+import { UserService } from './../../services/user.service';
+import { SpotifyUserService } from './../../services/spotify-user.service';
 import { ServiceBase } from './../../services/service.base';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-
-declare var Spotify: any;
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +13,28 @@ declare var Spotify: any;
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private auth: AuthService, private service: ServiceBase) { }
+  constructor(private serviceUsuario: SpotifyUserService, private usuario: UserService) { }
+
+  userPlaylists: any[] = [];
+  userAlbums: any[] = [];
+  userTracks: any[] = [];
+
+  usuarioLogado: User;
 
   ngOnInit() {
-    
+    let requests = [];
+
+    requests.push(this.serviceUsuario.getUserPlaylists(), this.serviceUsuario.getUserAlbums(), this.serviceUsuario.getUserTracks());
+    this.usuario.getUser()
+      .subscribe(item => {
+        this.usuarioLogado = item;
+      });
+
+    forkJoin(requests)
+      .subscribe((items: any[]) => {
+        this.userPlaylists = items[0].items;
+        this.userAlbums = items[1].items;
+        this.userTracks = items[2].items;
+      });
   }
 }
