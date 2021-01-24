2d704b64144750719d663c5e8594d0944e430a64
Matheus Kruger <ms-kruger@hotmail.com>
2021-01-23 21:58:55 -0300

    fix error on put method, passing some dummy data to it

--- src/app/services/spotify-player.service.ts
+++ src/app/services/spotify-player.service.ts
@@ -112,7 +112,7 @@ export class SpotifyPlayerService {
     }
 
     suffle(state: boolean, device_id: string) {
-        return this.service.Put("https://api.spotify.com/v1/me/player/shuffle?" + "state=" + state + "&device_id=" + device_id);
+        return this.service.Put("https://api.spotify.com/v1/me/player/shuffle?" + "state=" + state + "&device_id=" + device_id, {});
     }
 
     previous(device_id: string) {
