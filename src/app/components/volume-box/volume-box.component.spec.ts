823dd948d2c5c74903c27824f093e28b5d033ab8
Matheus da Silva Kruger <matheusk@ciandt.com>
2021-03-04 08:07:16 -0300

    revolving tests

--- src/app/components/volume-box/volume-box.component.spec.ts
+++ src/app/components/volume-box/volume-box.component.spec.ts
@@ -1,9 +1,7 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { VolumeBoxComponent } from './volume-box.component';
+import { HttpClient } from '@angular/common/http';
 
 describe('VolumeBoxComponent', () => {
   let component: VolumeBoxComponent;
@@ -11,7 +9,8 @@ describe('VolumeBoxComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ VolumeBoxComponent ]
+      declarations: [ VolumeBoxComponent ],
+      imports: [HttpClient]
     })
     .compileComponents();
   }));
