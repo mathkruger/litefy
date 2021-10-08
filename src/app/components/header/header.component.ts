f8d5e86f94a981a39591da997f2f33720f98d748
sharoonIlyas <sharoon.ilyas94@gmail.com>
2021-10-08 14:53:13 +0200

    fixed userMenu clicked outside

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,38 +1,51 @@
-import { Router } from '@angular/router';
-import { AuthService } from './../../services/auth.service';
-import { UserService } from './../../services/user.service';
-import { User } from './../../models/user';
-import { Component, Injector, OnInit } from '@angular/core';
-import { SettingsBase } from 'src/app/models/base/settings-base';
+import {Router} from '@angular/router';
+import {AuthService} from './../../services/auth.service';
+import {UserService} from './../../services/user.service';
+import {User} from './../../models/user';
+import {Component, ElementRef, HostListener, Injector, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {SettingsBase} from 'src/app/models/base/settings-base';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent extends SettingsBase implements OnInit {
+    constructor(
+        private userService: UserService,
+        public auth: AuthService,
+        private router: Router,
+        private ele: ElementRef,
+        injector: Injector) {
+        super(injector);
+    }
+    user: User;
+    openMenu = false;
 
-  constructor(private userService: UserService, public auth: AuthService, private router: Router, injector: Injector) {
-    super(injector);
-  }
+    userOpenMenu = false;
 
-  user: User;
-  openMenu = false;
+    ngOnInit() {
+        this.userService.getUser().subscribe(item => {
+            this.user = item;
+        });
 
-  userOpenMenu = false;
+        super.init();
+    }
 
-  ngOnInit() {
-    this.userService.getUser().subscribe(item => {
-      this.user = item;
-    });
+    sair() {
+        this.userOpenMenu = false;
+        this.auth.logout();
+        this.router.navigate(['login']).then();
+    }
 
-    super.init();
-  }
+    @HostListener('document:click', ['$event'])
+    clicked_outside(event) {
+        if (!this.ele.nativeElement.contains(event.target)) {
+            this.userOpenMenu = false;
+        }
+    }
 
-  sair() {
-    this.userOpenMenu = false;
-    this.auth.logout();
-    this.router.navigate(['login']);
-  }
-
+    closeMenu() {
+        this.userOpenMenu = false;
+    }
 }
