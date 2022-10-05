d7f8867f782ff28f551cf960c9dc83e4e02dd4a1
suraj yadav <surajyr93@gmail.com>
2022-10-05 10:33:58 +0530

    update to angular from 10v to 11v

--- src/app/components/youtube-player/youtube-player.component.spec.ts
+++ src/app/components/youtube-player/youtube-player.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { DebugElement } from "@angular/core";
 
@@ -9,7 +9,7 @@ describe("YoutubePlayerComponent", () => {
     let component: YoutubePlayerComponent;
     let fixture: ComponentFixture<YoutubePlayerComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [YoutubePlayerComponent],
         }).compileComponents();
